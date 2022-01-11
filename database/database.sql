4e62489335b1e0bc661f3ddd68808bc2e652b7fe
aliomid730 <ali.omid730@gmail.com>
2022-01-11 11:49:24 +0430

    Update database.sql

--- database/database.sql
+++ database/database.sql
@@ -1,5 +1,4 @@
 SET CHARSET UTF8;
 CREATE DATABASE IF NOT EXISTS cacti CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-CREATE USER 'cacti'@'%' IDENTIFIED BY 'Paccw0rd';
 grant all privileges on cacti.* to 'cacti'@'%';
 flush privileges;
